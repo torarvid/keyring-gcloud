08551c325bb049a5604eaa4d84998fdc1be059b6
Tor Arvid Lund <torarvid@gmail.com>
2024-09-26 20:15:28 +0200

    Let's see if the initial code works on the first try

---
+++ README.md
@@ -0,0 +1,79 @@
+# keyring-gcloud
+
+A keyring backend for Google Cloud Platform.
+
+## Installation
+
+We recommend using [uv](https://github.com/astral-sh/uv) to install this keyring
+backend.
+
+```bash
+uv tool install keyring --with keyring-gcloud
+```
+
+## How it works
+
+This backend does not store any credentials by itself. It will choose a
+storage-backend by looking at all viable backends and choose the one with the
+highest priority. It works by intercepting invocations of `keyring get|set`. A
+`get` operation that is intercepted works like this:
+
+1. Attempt to get the value from the storage backend
+2. Decode this value as if it was written by **this backend**
+   1. If decoding successful, check the expiry of the token
+      1. If not expired, return the token.
+   2. If decoding unsuccessful, use google-auth to fetch a new token (similar
+      to doing `gcloud auth print-access-token`)
+      1. Store the new token in the storage backend
+      2. Return the new token
+
+A `set` operation is simpler. It will just prepend an expiry of 1 hour to the
+supplied token, encode these two values and store them in the storage backend.
+
+## Usage
+
+There are two ways to use this backend:
+
+### 1: Via the `keyring` command line parameters:
+
+AKA the "I'll use it on-demand, thank you very much" method.
+
+```bash
+export KEYRING_GCLOUD_ON=1_or_yes_or_any_string_really
+keyring --keyring-backend keyring_gcloud.GoogleCloudKeyring <...>
+```
+
+The env variable `KEYRING_GCLOUD_ON` will make this backend intercept any
+invocation.
+
+### 2: Via the keyring configuration file:
+
+In the keyring configuration file, add the following:
+
+```toml
+[backend]
+default-keyring=keyring_gcloud.GoogleCloudKeyring
+```
+
+This will make `keyring` use the `GoogleCloudKeyring` backend on all calls to
+`keyring get foo bar` (regardless of any `--keyring-backend` parameter). This
+has some risk, since if you were to run
+
+```bash
+keyring set some-website foo@example.com mypassword
+```
+
+it is unlikely that you would want `mypassword` to have an expiry of 1 hour. To
+lower this risk, you should **unset** the `KEYRING_GCLOUD_ON` environment
+variable. When that env variable is **not set**, the backend only intercepts if
+the `username` for the request matches `KEYRING_GCLOUD_USERNAME` (default
+`oauth2accesstoken`).
+
+So a call like
+
+```bash
+keyring get https://private-pypi.example.com/simple/ oauth2accesstoken
+```
+
+would be intercepted (poetry is an example of a service that does this with
+`oauth2accesstoken` as the username).
---
+++ keyring_gcloud/__init__.py
@@ -0,0 +1,146 @@
+"""
+A keyring backend for Google Cloud Platform.
+
+It uses another keyring as an actual storage backend. This backend is only a proxy
+that encodes/decodes the credentials and refreshes it if we believe it's expired.
+
+Google access tokens are opaque, so we can't inspect them to evaluate actual
+expiration. So we basically use 1 hour "because people on the internet said so".
+"""
+
+import base64
+from datetime import datetime, timedelta, timezone
+from typing import Self
+from keyring.backend import KeyringBackend, get_all_keyring
+from google.auth.transport.requests import Request
+from google.auth.credentials import Credentials
+import google.auth
+from dataclasses import dataclass
+from os import environ
+
+try:
+    from keyring.compat import properties
+except ImportError:
+    # Workaround for Poetry up to at least 1.8.3
+    from keyring._compat import properties
+
+get_all_keyring()
+
+# Refresh token if it's older than this
+default_expiry = timedelta(minutes=55)  # 1 hour minus a little bit
+
+
+@dataclass
+class GoogleCredential:
+    token: str | None = None
+    expiry: datetime | None = None
+
+    @property
+    def valid(self) -> bool:
+        return (
+            self.token is not None
+            and self.expiry is not None
+            and self.expiry >= datetime.now(timezone.utc)
+        )
+
+    @classmethod
+    def from_encoded(cls, encoded_credentials: str | None) -> Self:
+        """
+        Decodes the encoded credentials and returns a new instance.
+
+        Encoded format is "<expiry>:<token>" where both are base64 encoded.
+        <expiry> is a datetime in ISO format and <token> is the access token as a
+        string.
+        """
+        try:
+            assert encoded_credentials, "whoops. fallback to except handler..."
+            encoded_expiry, encoded_token = encoded_credentials.split(":")
+            expiry_str = base64.b64decode(encoded_expiry).decode("ascii")
+            expiry = datetime.fromisoformat(expiry_str)
+            token = base64.b64decode(encoded_token).decode("ascii")
+            return cls(token, expiry)
+        except Exception:
+            return cls()
+
+    def refresh(self) -> None:
+        credentials: Credentials
+        credentials, _ = google.auth.default()  # type: ignore
+
+        # If credentials are expired, refresh them
+        if credentials.expired:
+            credentials.refresh(Request())
+
+        # Get the access token
+        self.token = credentials.token  # type: ignore
+        self.expiry = datetime.now(timezone.utc) + default_expiry
+
+    def encode(self) -> str:
+        """
+        Encodes the credentials and returns it as a string.
+
+        Encoded format is "<expiry>:<token>" where both are base64 encoded.
+        <expiry> is a datetime in ISO format and <token> is the access token as a
+        string.
+        """
+        assert self.token and self.expiry
+
+        access_token_bytes = self.token.encode("ascii")
+        encoded_token = base64.b64encode(access_token_bytes).decode("ascii")
+
+        expiry_bytes = self.expiry.isoformat().encode("ascii")
+        encoded_expiry = base64.b64encode(expiry_bytes).decode("ascii")
+
+        encoded_credentials = f"{encoded_expiry}:{encoded_token}"
+        return encoded_credentials
+
+
+class GoogleCloudKeyring(KeyringBackend):
+    backend: KeyringBackend
+
+    def __init__(self):
+        super().__init__()
+        self._set_backend()
+
+    def _set_backend(self):
+        viable_backends = sorted(self.get_viable_backends(), key=lambda b: -b.priority)
+        viable_backends = [
+            b for b in viable_backends if not isinstance(b, GoogleCloudKeyring)
+        ]
+        if len(viable_backends) < 1:
+            raise Exception("No viable backends")
+
+        self.backend = viable_backends[0]()
+
+    @properties.classproperty
+    def priority(self) -> float:
+        return 1
+
+    def get_password(self, service: str, username: str) -> str | None:
+        secret = self.backend.get_password(service, username)
+        if not self._should_intercept(service, username):
+            return secret
+
+        cred = GoogleCredential.from_encoded(secret)
+        if not cred.valid:
+            cred.refresh()
+            self.backend.set_password(service, username, cred.encode())
+        return cred.token
+
+    def set_password(self, service: str, username: str, password: str) -> None:
+        if self._should_intercept(service, username):
+            expiry = datetime.now(timezone.utc) + default_expiry
+            cred = GoogleCredential(password, expiry=expiry)
+            password = cred.encode()
+        self.backend.set_password(service, username, password)
+
+    def delete_password(self, service: str, username: str) -> None:
+        self.backend.delete_password(service, username)
+
+    def _should_intercept(self, service: str, username: str) -> bool:
+        always_intercept = len(environ.get("KEYRING_GCLOUD_ON", "")) > 0
+        if always_intercept:
+            return True
+
+        # TODO consider making this less dumb
+        special_username = environ.get("KEYRING_GCLOUD_USERNAME", "oauth2accesstoken")
+        return username == special_username
